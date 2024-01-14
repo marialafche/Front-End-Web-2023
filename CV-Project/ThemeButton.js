6378e0e2ec4945399d1d7f3b2424655b99f70bbd
marialafche <marialafche@gmail.com>
2024-01-14 21:58:50 +0200

    EventListener

--- CV-Project/ThemeButton.js
+++ CV-Project/ThemeButton.js
@@ -1,5 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    window.changeTheme = function() {
-        document.body.classList.toggle("theme");
-    };
-});
+const button = document.getElementById("theme-button");
+
+function changeTheme(){
+    document.body.classList.toggle("theme");
+}
+
+button.addEventListener("click", changeTheme);
