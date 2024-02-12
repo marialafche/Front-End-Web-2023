868e58852870447178bb065cf5e0b4ff5c265205
Maria Lafchieva <marialafche@gmail.com>
2024-02-12 21:58:05 +0200

    New Photos

---
+++ Delivery-Site-Project/scripts/order.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', ()=>{
+    const fancyMenu = document.getElementsByClassName("fancyMenu")[0]
+    .addEventListener('click', ()=>{
+        location.href = '../html/fancyMenu.html'
+    });
+});
+
+window.addEventListener('load', ()=>{
+    const sushiMenu = document.getElementsByClassName("sushiMenu")[0]
+    .addEventListener('click', ()=>{
+        location.href = '../html/sushiMenu.html'
+    });
+});
+
+window.addEventListener('load', ()=>{
+    const fastFoodMenu = document.getElementsByClassName("fastFoodMenu")[0]
+    .addEventListener('click', ()=>{
+        location.href = '../html/fastFoodMenu.html'
+    });
+});
+
+window.addEventListener('load', ()=>{
+    const italianMenu = document.getElementsByClassName("italianMenu")[0]
+    .addEventListener('click', ()=>{
+        location.href = '../html/italianMenu.html'
+    });
+});
+
+
+
